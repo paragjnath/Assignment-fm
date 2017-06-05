29584b23c5a650ea3d0520ddd96b7d9aefdb1449
paragjnath <paragjnath@gmail.com>
2017-06-06 00:48:21 +0530

    Added menu activity

--- app/src/main/java/com/fancymonk/fancymonk/activity/MenuActivity.java
+++ app/src/main/java/com/fancymonk/fancymonk/activity/MenuActivity.java
@@ -2,10 +2,13 @@ package com.fancymonk.fancymonk.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import com.fancymonk.fancymonk.R;
+import com.fancymonk.fancymonk.adapter.MenuRecyclerViewAdapter;
 import com.fancymonk.fancymonk.model.Menu;
 
 import java.util.ArrayList;
@@ -13,6 +16,8 @@ import java.util.List;
 
 public class MenuActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
     private List<Menu> mMenu;
     private Button btnConfirm;
 
@@ -29,5 +34,13 @@ public class MenuActivity extends AppCompatActivity {
             Menu menuItem = new Menu("Menu item "+i, "Menu description "+i, i*10.0,false);
             mMenu.add(menuItem);
         }
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.menuRecyclerView);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mAdapter = new MenuRecyclerViewAdapter(mMenu);
+
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
